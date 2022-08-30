66e9b809ce54640f1fa36e1ffe4a9b83df52d29b
zigad <zigadario@gmai.com>
2022-08-30 19:22:17 +0200

    Inicial commit.

---
+++ PoslovnaInformatikaProject/src/test/java/com/example/PoslovnaProjekat/PoslovnaInformatikaProject/PoslovnaInformatikaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PoslovnaProjekat.PoslovnaInformatikaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaInformatikaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
